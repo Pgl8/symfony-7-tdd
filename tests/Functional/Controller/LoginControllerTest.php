3cdfc1dfc723aad2e7699d313c1b0f5a4f160e35
Jose García <pepe.gl@gmail.com>
2024-04-11 19:24:07 +0200

    Add Register and tests

--- tests/Functional/Controller/LoginControllerTest.php
+++ tests/Functional/Controller/LoginControllerTest.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class LoginControllerTest extends WebTestCase
 {
-    public function testLogin(): void
+    public function testLoginIsShown(): void
     {
         // This calls KernelTestCase::bootKernel(), and creates a
         // "client" that is acting as the browser
@@ -22,5 +22,35 @@ class LoginControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h1', 'Please sign in');
 
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Please sign in")')->count());
+    }
+
+    public function testRegisterIsShown(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('p', "Don't have an account yet?");
+    }
+
+    public function testRegisterForm(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/register');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Create your account');
+
+        $form = $crawler->selectButton('Register')->form();
+
+        $form['registration_form[username]'] = 'test';
+        $form['registration_form[email]'] = 'test@test.com';
+        $form['registration_form[plainPassword]'] = 'password1234';
+        $form['registration_form[agreeTerms]'] = true;
+
+        $client->submit($form);
+        $client->followRedirect();
+        $this->assertSelectorTextContains('div', 'You are logged in as test@test.com');
+
     }
 }
